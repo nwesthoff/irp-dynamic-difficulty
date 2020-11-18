09c728dddbfce58638b291fc4218fa967d480b1c
nwesthoff <nilsepils94@hotmail.com>
2020-11-18 14:37:50 +0100

    📝 Lots-o-writing

--- config/MyReferences.ts
+++ config/MyReferences.ts
@@ -9,6 +9,16 @@ const myReferences: AcademicRef[] = [
         "Nacke, L. and Lindley, C.A. (2008). Flow and Immersion in first-person Shooters. Proceedings of the 2008 Conference on Future Play Research, Play, Share - Future Play ’08.",
     },
   },
+  {
+    citationKey: "sugar_2019_the",
+    entryType: "TRUSTED",
+    entryTags: {
+      author: "Sugar",
+      year: "2019",
+      inBib:
+        "Sugar, R. (2019). The World Is chaos. Escape Rooms Make sense. [online] Vox. Available at: https://www.vox.com/the-goods/2019/8/7/20749177/escape-room-game [Accessed 18 Nov. 2020].",
+    },
+  },
 ];
 
 export interface AcademicRef {
