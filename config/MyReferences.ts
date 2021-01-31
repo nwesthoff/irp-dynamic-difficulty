2d923d04646c8d2f5b4bf8e32da0b8dcc26618bc
Nils Westhoff <nwesthoff@conceptivate.nl>
2021-01-31 16:18:18 +0100

    📝 Add citations

--- config/MyReferences.ts
+++ config/MyReferences.ts
@@ -29,6 +29,157 @@ const myReferences: AcademicRef[] = [
         "Bates, B. (1997). Designing the Puzzle. [online] www.scottkim.com.previewc40.carrierzone.com. Available at: http://www.scottkim.com.previewc40.carrierzone.com/thinkinggames/GDC00/bates.html [Accessed 23 Nov. 2020].",
     },
   },
+
+  {
+    citationKey: "cable_2018_escape",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Cable",
+      year: "2018",
+      inBib:
+        "Cable, L. (2018). Escape Games for Large Groups. [Recording] Available at: https://upthegame.nl/watch-talks/ [Accessed 19 Oct. 2020].",
+    },
+  },
+  {
+    citationKey: "f",
+    entryType: "ACADEMIC",
+    entryTags: {
+      author: "Chen",
+      year: "2007",
+      inBib:
+        "Chen, J. (2007). Flow in Games (and Everything else). Communications of the ACM, 50(4), p.31.",
+    },
+  },
+  {
+    citationKey: "csikszentmihalyi_2008_",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Csikszentmihalyi",
+      year: "2008",
+      inBib:
+        "Csikszentmihalyi, M. (2008). Flow: The Psychology of Optimal Experience. New York: Harper & Row.",
+    },
+  },
+  {
+    citationKey: "elumir_2018_13",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Elumir",
+      year: "2018",
+      inBib:
+        "Elumir, E. (2018). 13 Rules for Escape Room Puzzle Design. [online] The Codex. Available at: https://thecodex.ca/13-rules-for-escape-room-puzzle-design/#Rule_1_Puzzles_Should_Be_Fair_You_are_on_the_Players_Side [Accessed 31 Jan. 2021].",
+    },
+  },
+  {
+    citationKey: "elumir_2019a_2019",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Elumir",
+      year: "2019a",
+      inBib:
+        "Elumir, E. (2019a). 2019 Escape Room Enthusiast Survey. [online] The Codex. Available at: https://thecodex.ca/2019-escape-room-enthusiast-survey/ [Accessed 31 Jan. 2021].",
+    },
+  },
+  {
+    citationKey: "elumir_2019b_how",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Elumir",
+      year: "2019b",
+      inBib:
+        "Elumir, E. (2019b). How Escape Rooms Do Difficulty Wrong. [online] The Codex. Available at: https://thecodex.ca/how-escape-rooms-do-difficulty-wrong-the-player-loop/ [Accessed 13 Jul. 2020].",
+    },
+  },
+  {
+    citationKey: "feldman-barrett_2020_how",
+    entryType: "TRUSTED",
+    entryTags: {
+      author: "Feldman Barrett",
+      year: "2020",
+      inBib:
+        "Feldman Barrett, L. and University of Waikato (2020). How Emotions Are Made: The Secret Life of the Brain. YouTube. Available at: https://www.youtube.com/watch?v=KliAI9umFyY [Accessed 30 Nov. 2020].",
+    },
+  },
+  {
+    citationKey: "game-maker's-toolkit_2020_the",
+    entryType: "TRUSTED",
+    entryTags: {
+      author: "Game Maker's Toolkit",
+      year: "2020",
+      inBib:
+        "Game Maker's Toolkit (2020). The Psychological Trick That Can Make Rewards Backfire. YouTube. Available at: https://www.youtube.com/watch?v=1ypOUn6rThM [Accessed 11 Oct. 2020].",
+    },
+  },
+  {
+    citationKey: "heikkinen-and-shumeyko_2016_designing",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Heikkinen and Shumeyko",
+      year: "2016",
+      inBib:
+        "Heikkinen, O. and Shumeyko, J. (2016). Designing an Escape Room with the Experience Pyramid Model.",
+    },
+  },
+  {
+    citationKey: "lazarro_2016_games",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Lazarro",
+      year: "2016",
+      inBib:
+        "Lazarro, N. and AIGA Design (2016). Games and the Four Keys to Fun: Using Emotions to Create Engaging Design. YouTube. Available at: https://www.youtube.com/watch?v=EEmNRRRqgNc [Accessed 10 Nov. 2020].",
+    },
+  },
+  {
+    citationKey: "lazarro_2004_why",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Lazarro",
+      year: "2004",
+      inBib:
+        "Lazarro, N. and XEO Design (2004). Why We Play Games: Four Keys to More Emotion without Story. [online] XEO Design. Available at: http://xeodesign.com/xeodesign_whyweplaygames.pdf [Accessed 14 Nov. 2020].",
+    },
+  },
+  {
+    citationKey: "moran_2017_tools",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Moran",
+      year: "2017",
+      inBib:
+        "Moran, N. (2017). Tools of Immersion. [Recorded in-person] Available at: https://upthegame.nl/watch-talks/ [Accessed 19 Oct. 2020].",
+    },
+  },
+  {
+    citationKey: "schell_2019_the",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Schell",
+      year: "2019",
+      inBib:
+        "Schell, J. (2019). The Art of Game Design: a Book of Lenses. Boca Raton: Taylor & Francis, a Crc Title, Part of the Taylor & Francis Imprint, a Member of the Taylor & Francis Group, the Academic Division of T&F Informa, Plc.",
+    },
+  },
+  {
+    citationKey: "spira_2020_6",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Spira",
+      year: "2020",
+      inBib:
+        "Spira, L. (2020). 6 Year US Escape Room Industry Report (August 2020). [online] Room Escape Artist. Available at: https://roomescapeartist.com/2020/08/24/escape-room-industry-report-2020/ [Accessed 31 Jan. 2021].",
+    },
+  },
+  {
+    citationKey: "wiemker,-elumir-and-clare_2015_escape",
+    entryType: "TRADEMAG",
+    entryTags: {
+      author: "Wiemker, Elumir and Clare",
+      year: "2015",
+      inBib:
+        "Wiemker, M., Elumir, E. and Clare, A. (2015). Escape Room Games: Can you transform an unpleasant situation into a pleasant one? [online] Available at: https://thecodex.ca/wp-content/uploads/2016/08/00511Wiemker-et-al-Paper-Escape-Room-Games.pdf [Accessed 31 Jan. 2021].",
+    },
+  },
 ];
 
 export interface AcademicRef {
