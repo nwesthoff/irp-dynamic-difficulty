c2b3522f6b1c0cb432d553548ba466248337bb75
Nils Westhoff <nwesthoff@conceptivate.nl>
2021-02-01 13:17:57 +0100

    ✨ Create side-by-side display components

---
+++ components/MDXComponents/SideBySide.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement, ReactNode } from "react";
+import styled from "styled-components";
+
+const StyledSideBySide = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-flow: row wrap;
+  gap: 2rem;
+
+  & > figure {
+    flex-basis: 33%;
+    min-width: 280px;
+    flex-grow: 1;
+  }
+`;
+
+interface Props {
+  children: ReactNode;
+}
+
+export default function SideBySide({ children }: Props): ReactElement {
+  return <StyledSideBySide>{children}</StyledSideBySide>;
+}
